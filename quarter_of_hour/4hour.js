e15960645151b466347f534ad17dcfa87177af55
fAyday-everyday <polkiju489@gmaill.com>
2021-12-20 19:45:35 +0600

    1/4 of hour HM added

---
+++ quarter_of_hour/4hour.js
@@ -0,0 +1,14 @@
+let min = 56;
+if (min >= 0 && min <= 14) {
+    alert('First half hour');
+}
+if (min >= 15 && min <= 29) {
+    alert('Second half hour');
+}
+if (min >= 30 && min <= 44) {
+    alert('Third half hour');
+}
+if (min >= 45 && min <= 60) {
+    alert('Fourth half hour');
+};
+alert(min);
