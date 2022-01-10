28ad969cb965de1ebd666d4133a8cc23edbfd2fd
fAyday-everyday <polkiju489@gmaill.com>
2022-01-10 21:45:31 +0600

    function-HM added

---
+++ function-HM/function-HM.js
@@ -0,0 +1,31 @@
+
+//  mul(n, m)
+
+let n = 12;
+let m = 9;
+
+function mul (n, m) {}
+console.log( n * m );
+
+
+//   invertion
+
+function reverse (str) {
+    return str.split('').reverse().join('');
+}
+
+console.log(reverse('12345'));
+
+
+//  length
+
+function longname([USA, France, Switzerland, Portugal]) {
+    let longname = str.split(' ');
+    let longestWord = 0;
+    for(let i = 0; i < strSplit.length; i++){
+      if(strSplit[i].length > longestWord){
+	longestWord = strSplit[i].length;
+     }
+  }
+  return longestWord;
+}
