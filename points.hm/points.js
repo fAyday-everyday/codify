c1f15fb21a8d8ae16cc6f81aee81c5f5648202e3
fAyday-everyday <polkiju489@gmaill.com>
2022-01-10 20:55:37 +0600

    'quiz' added

---
+++ points.hm/points.js
@@ -0,0 +1,27 @@
+let point = 0;
+let answer = ' ';
+
+ answer = prompt('11x3?');
+
+if (answer === '33') {
+    alert('right');
+    point++;
+} else {
+    if (answer != '33') {
+        alert('wrong')
+    }
+}
+
+answer = prompt('how many corners does a square have?');
+
+if (answer === '4') {
+    alert('right');
+    point++;
+} else {
+    if (answer != '4') {
+        alert('wrong')
+    }
+}
+
+alert('you got ' + point + ' points');
+
